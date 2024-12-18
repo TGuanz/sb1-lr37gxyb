34537fde214b878edbd8298389cbac3093edb155
TGuanz <support@mybatteryplus.com.au>
2024-12-19 06:45:47 +1100

    Update airtableService.ts

--- src/services/airtableService.ts
+++ src/services/airtableService.ts
@@ -1,6 +1,4 @@
-const AIRTABLE_API_KEY = 'patWa7h4RrHArJydB.395e6344c19cd8938b6527e6cdb60631ea0e2bf74c3e52cee5700466fba0bc3b';
-const BASE_ID = 'app1ECw6zKtn8F4lW';
-const TABLE_ID = 'tblVv3WDauNbHDK8B';
+
 
 export interface AirtableRecord {
   id: string;
